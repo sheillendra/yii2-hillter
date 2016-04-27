2f431f9bf267cda3f89b1e28aece610687aa3fa3
suryana-itconcept <suryana@itconcept.sg>
2016-04-27 12:44:44 +0700

    init

--- assets/HillterAsset.php
+++ assets/HillterAsset.php
@@ -1,20 +1,23 @@
 <?php
 
-namespace frontend\themes\hillter\assets;
+namespace sheillendra\hillter\assets;
 
 use yii\web\AssetBundle;
 
 class HillterAsset extends AssetBundle {
-    public $sourcePath = '@frontend/themes/hillter/assets/template';
+    public $sourcePath = '@sheillendra/hillter/assets/template';
     public $css = [
-        
+        'css/style.css',
+        'css/custom.css',
     ];
     public $js = [
-        
+        'js/scripts.js',
     ];
     public $depends = [
         'yii\web\JqueryAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
         'yii\web\YiiAsset',
+        'sheillendra\hillter\assets\FontAwesomeAsset',
     ];
     //public $publishOptions=['forceCopy'=>YII_DEBUG];
 }
