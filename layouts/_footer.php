70b30746abc905868be0a77d75f40c58d042e2a8
suryana-itconcept <suryana@itconcept.sg>
2016-08-19 08:42:20 +0700

    init

---
+++ layouts/_footer.php
@@ -0,0 +1,103 @@
+<!-- FOOTER -->
+<footer id="footer">
+
+    <!-- FOOTER TOP -->
+    <div class="footer_top">
+        <div class="container">
+            <div class="row">
+
+                <!-- WIDGET MAILCHIMP -->
+                <div class="col-lg-8">
+                    <div class="mailchimp">
+                        <h4>News &amp; Offers</h4>
+                        <div class="mailchimp-form">
+                            <form action="#" method="POST">
+                                <input type="text" name="email" placeholder="Your email address" class="input-text">
+                                <button class="awe-btn">SIGN UP</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- END / WIDGET MAILCHIMP -->
+
+                <!-- WIDGET SOCIAL -->
+                <div class="col-lg-3 col-lg-offset-1">
+                    <div class="social">
+                        <div class="social-content">
+                            <a href="#"><i class="fa fa-pinterest"></i></a>
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-google-plus"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <!-- END / WIDGET SOCIAL -->
+
+            </div>
+        </div>
+    </div>
+    <!-- END / FOOTER TOP -->
+
+    <!-- FOOTER CENTER -->
+    <div class="footer_center">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-xs-12 col-lg-5">
+                    <div class="widget widget_logo">
+                        <div class="widget-logo">
+                            <div class="img">
+                                <a href="#"><img src="<?php echo $hillterAsset->baseUrl ?>/images/logo-footer.png" alt=""></a>
+                            </div>
+                            <div class="text">
+                                <p><i class="hillter-icon-location"></i> 225 Beach Street, Australian</p>
+                                <p><i class="hillter-icon-phone"></i> 1-548-854-8898</p>
+                                <p><i class="fa fa-envelope-o"></i> <a href="mailto:hillterhotel@gmail.com">hillterhotel@gmail.com</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xs-4 col-lg-2">
+                    <div class="widget">
+                        <h4 class="widget-title">Services</h4>
+                        <ul>
+                            <li><a href="#">Guest Book</a></li>
+                            <li><a href="#">Gallery</a></li>
+                            <li><a href="#">Restaurant</a></li>
+                            <li><a href="#">Event</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-xs-4 col-lg-2">
+                    <div class="widget">
+                        <h4 class="widget-title">ABOUT</h4>
+                        <ul>
+                            <li><a href="">About</a></li>
+                            <li><a href="">Blog</a></li>
+                            <li><a href="">Contact Us</a></li>
+                            <li><a href="">Comming Soon</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-xs-4 col-lg-3">
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- END / FOOTER CENTER -->
+
+    <!-- FOOTER BOTTOM -->
+    <div class="footer_bottom">
+        <div class="container">
+            <p>&copy; 2015 Hillter Hotel All rights reserved.</p>
+        </div>
+    </div>
+    <!-- END / FOOTER BOTTOM -->
+
+</footer>
+<!-- END / FOOTER -->
---
+++ layouts/_header.php
@@ -0,0 +1,70 @@
+<!-- HEADER -->
+<header id="header">
+
+    <!-- HEADER TOP -->
+    <div class="header_top">
+        <div class="container">
+            <div class="header_left float-left">
+                <span class="login-register">
+                    <a href="login.html">Login</a>
+                    <a href="register.html">register</a>
+                </span>
+            </div>
+            <!--
+            <div class="header_right float-right">
+                <div id="google_translate_element"></div>
+                <script type="text/javascript">
+                    function googleTranslateElementInit() {
+                        new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+                    }
+                </script>
+                <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+            </div>-->
+        </div>
+    </div>
+    <!-- END / HEADER TOP -->
+
+    <!-- HEADER LOGO & MENU -->
+    <div class="header_content" id="header_content">
+
+        <div class="container">
+            <!-- HEADER LOGO -->
+            <div class="header_logo">
+                <a href="#"><img src="<?php echo $hillterAsset->baseUrl ?>/images/logo-header.png" alt=""></a>
+            </div>
+            <!-- END / HEADER LOGO -->
+
+            <!-- HEADER MENU -->
+            <nav class="header_menu">
+                <ul class="menu">
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="about.html">About</a>
+                    </li>
+                    <li>
+                        <a href="about.html">Services</a>
+                    </li>
+                    <li>
+                        <a href="about.html">Gallery</a>
+                    </li>
+                    <li>
+                        <a href="about.html">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- END / HEADER MENU -->
+
+            <!-- MENU BAR -->
+            <span class="menu-bars">
+                <span></span>
+            </span>
+            <!-- END / MENU BAR -->
+
+        </div>
+    </div>
+    <!-- END / HEADER LOGO & MENU -->
+
+</header>
+<!-- END / HEADER -->
